b98287ad7f691612c7c3487446e3d71b186baebe
SowmiyaRaja <34887465+SowmiyaRaja@users.noreply.github.com>
2024-11-02 19:37:24 +0530

    Update Alerter.cs

--- Alerter.cs
+++ Alerter.cs
@@ -3,26 +3,26 @@ public class Alerter
         TO_CONTROLLER, 
         TO_EMAIL }
 
-    public static void checkAndAlert(AlertTarget alertTarget, BreachDetector.CoolingType coolingType, double temperatureInC)
+    public static void checkAndAlert(AlertTarget alertTarget, BreachFinder.CoolingType coolingType, double temperatureInC)
     {
-        BreachDetector.BreachType breachType = BreachDetector.classifyTemperatureBreach(coolingType, temperatureInC);
+        BreachFinder.BreachType breachType = BreachFinder.classifyTemperatureBreach(coolingType, temperatureInC);
         if (alertTarget == AlertTarget.TO_CONTROLLER)
             sendToController(breachType);
         else
             sendToEmail(breachType);
     }
 
-    private static void sendAlertToController(BreachDetector.BreachType breachType)
+    private static void sendAlertToController(BreachFinder.BreachType breachType)
     {
       const ushort header = 0xfeed;
       Console.WriteLine("{} : {}\n", header, breachType);
     }
 
-    private static void sendAlertToEmail(BreachDetector.BreachType breachType)
+    private static void sendAlertToEmail(BreachFinder.BreachType breachType)
     {
         String recipient = "a.b@c.com";
-        if (breachType != BreachDetector.BreachType.NORMAL)
+        if (breachType != BreachFinder.BreachType.NORMAL)
             Console.WriteLine("To: {0}\n Hi, the temperature is {1}\n", recipient,
-                breachType == BreachDetector.BreachType.TOO_LOW ? "too low" : "too high");
+                breachType == BreachFinder.BreachType.TOO_LOW ? "too low" : "too high");
     }
 }
